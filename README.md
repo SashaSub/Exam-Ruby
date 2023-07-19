d7013611a652eb8db7c0c6663f833d1e6f9e8e65
SashaSub <ssuboch@gMAIL.com>
2023-07-19 15:36:25 -0400

    add ReadMe

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # README
 
+Exam task for the course Ruby on Rails
+Retail platform "Covid reatail"
+
+In order to place orders first you need to be registered and then the administrator must add you as a client.
+
+!!! El administrador agrega los usuarios como Clientes. !!!
+!!! Tiene que registrarse primero y luego el administrador debe agregarle como cliente. !!!
+
+For new users: first register a user, then the administrator can add you as a client.
+
+
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,24 @@
 #   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
 
-20.times do
+# !!! El administrador agrega los usuarios como Clientes. !!!
+# !!! Tiene que registrarse primero y luego el administrador debe agregarle como cliente. !!!
+
+    User.create!(email:"admin@gmail.com", password:"123456", name:"Admin", role:"admin")
+    User.create!(email:"user1@gmail.com", password:"123456", name:"User 1")
+    User.create!(email:"user2@gmail.com", password:"123456", name:"User 2")
+    User.create!(email:"user3@gmail.com", password:"123456", name:"User 3")
+    User.create!(email:"user3@gmail.com", password:"123456", name:"User 4")
+
+    20.times do
     Product.create!([
       { nombre: Faker::Commerce.product_name, precio: Faker::Commerce.price, descripcion: Faker::Lorem.paragraph(sentence_count: 2), cantidad: rand(100)}
     ])
-  end
+    end
 
-# !!! No usar para crear Clientes. Tiene que registrarse primero y luego el administrador debe agregarle como cliente. !!!
+    Payment.create!(nombre: "Efectivo")
+    Payment.create!(nombre: "Crédito")
+    Payment.create!(nombre: "Débito")  
 
-#   10.times do
-#     Client.create!([
-#       { nombre: Faker::Name.name, email: Faker::Internet.email, type_id: rand(1..2)}
-#     ])
-#   end
-
-  Payment.create!(nombre: "Efectivo")
-  Payment.create!(nombre: "Crédito")
-  Payment.create!(nombre: "Débito")  
-
-  Type.create!(nombre: "Minorista")
-  Type.create!(nombre: "Mayorista")
+    Type.create!(nombre: "Minorista")
+    Type.create!(nombre: "Mayorista")
